63e0f10bd050c879943b16da49d7ac432879bba2
s-ropalkar <siddhi75rop@gmail.com>
2025-08-15 23:09:56 +0530

    Create day1.java
    here is day 1 submission

---
+++ day1.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void sort012(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if (arr[mid] == 1) {
+                mid++;
+            }
+            else { // arr[mid] == 2
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 1, 0, 2, 1, 0};
+
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        sort012(arr);
+        System.out.println("Sorted Array:   " + Arrays.toString(arr));
+    }
+}
